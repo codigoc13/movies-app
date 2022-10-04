2d48ba64e2706145b979bfbd0502d26dcaa01704
Steven Zuluaga <steven000069@hotmail.com>
2022-10-04 18:31:07 -0500

    Agregando estilos diferentes a las movies

--- src/components/movie/MovieCard.jsx
+++ src/components/movie/MovieCard.jsx
@@ -4,15 +4,15 @@ const MovieCard = ({  movieInfo }) => {
      const PATH_URL ="https://image.tmdb.org/t/p/w500"
   return (
        <div  className="card-movie">
-            <p>
-                 {movieInfo.name}
-            </p>
+            
             {
                  movieInfo.poster_path
                       ? <img className='image' src={`${PATH_URL}${movieInfo.poster_path}`} alt={movieInfo.name} />
                       : null
             }
-            
+            <p>
+                 {movieInfo.name}
+            </p>
     </div>
   )
 }
