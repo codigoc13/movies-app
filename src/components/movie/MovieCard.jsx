cf6624d193e6eef44f855d2782a47a8f87ad0873
Steven Zuluaga <steven000069@hotmail.com>
2022-10-15 16:12:54 -0500

    Validando si la imagen esta en null

--- src/components/movie/MovieCard.jsx
+++ src/components/movie/MovieCard.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import './Movie.css'
-const MovieCard = ({  movieInfo }) => {
-     const PATH_URL ="https://image.tmdb.org/t/p/w500"
-  return (
-       <div  className="card-movie">
-            
-            {
-                 movieInfo.poster_path
-                      ? <img className='image' src={`${PATH_URL}${movieInfo.poster_path}`} alt={movieInfo.name} />
-                      : null
-            }
-            <p>
-                 {movieInfo.name}
-            </p>
-    </div>
-  )
-}
+import React from "react";
+import "./Movie.css";
+const MovieCard = ({ movieInfo }) => {
+     const PATH_URL = "https://image.tmdb.org/t/p/w500";
+     const image = movieInfo.poster_path ==="null"
+          ? movieInfo.backdrop_path
+          : movieInfo.poster_path
+     
+     return (
+          <div className="card-movie">
+               {
+                    image===null ? <img
+                         className="image"
+                         src="https://cdn.pixabay.com/photo/2017/11/24/10/43/ticket-2974645_960_720.jpg"
+                         alt={movieInfo.name}
+                    /> :
+                    <img
+                         className="image"
+                         src={`${PATH_URL}${image}`}
+                         alt={movieInfo.name}
+                    />
+               }
+               <p>{movieInfo.name}</p>
+          </div>
+     );
+};
 
-export default MovieCard
+export default MovieCard;
