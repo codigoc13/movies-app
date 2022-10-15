83da6625b1d5417b3c45c99ddcac5eddd7020155
Steven Zuluaga <steven000069@hotmail.com>
2022-10-15 16:12:36 -0500

    Agregando busqueda de series

--- src/App.js
+++ src/App.js
@@ -5,16 +5,32 @@ import MovieCard from './components/movie/MovieCard'
 
 const App = () => {
   const [movies, setMovies] = useState([])
-
+  const [searchKey, setSearchKey] = useState("")
   const API_URL = "https://api.themoviedb.org/3/"
 
   const fetchMovies = async () => {
+    
+    
+    const { data: { results } } = await axios.get(`${API_URL}discover/tv`,
+      {
+        params: {
+          api_key: process.env.REACT_APP_MOVIE_API_KEY,
+          
+        }
+      })
 
-    const { data: { results } } = await axios.get(`${API_URL}discover/tv`, {
-      params: {
-        api_key: process.env.REACT_APP_MOVIE_API_KEY
-      }
-    })
+
+    setMovies(results)
+
+  }
+  const fetchMoviesSearch = async (searchKey) => {
+    const { data: { results } } = await axios.get(`${API_URL}search/tv`,
+      {
+        params: {
+          api_key: process.env.REACT_APP_MOVIE_API_KEY,
+          query: searchKey
+        }
+      })
 
 
     setMovies(results)
@@ -25,22 +41,33 @@ const App = () => {
     fetchMovies()
   }, [])
 
- 
+  const searchMovies = (e) => {
+    e.preventDefault()
+    fetchMoviesSearch(searchKey)
+  }
+
   return (
     <div >
-      <h1>Hola Escuela de Código C13</h1>
+      <header>
+        <h1>Hola Escuela de Código C13</h1>
+        <form onSubmit={searchMovies}>
+
+          <input type='text' onChange={(e) => setSearchKey(e.target.value)} />
+          <button type='submit'>Submit</button>
+        </form>
+      </header>
       <div className='container-movies'>
         {
           movies.map(movie => (
-           <div key={movie.id}>
+            <div key={movie.id}>
               <MovieCard
-              
-              movieInfo={movie}
-            />
-           </div>
-              
-            
-            
+
+                movieInfo={movie}
+              />
+            </div>
+
+
+
 
 
           ))
