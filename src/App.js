de913ba4c3edb6316178f247397c73f2c3dfbddd
Steven Zuluaga <steven000069@hotmail.com>
2022-10-15 17:36:50 -0500

    Agregando movie container y aplicando estilos

--- src/App.js
+++ src/App.js
@@ -1,83 +1,79 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
-import MovieCard from './components/movie/MovieCard'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import MovieCard from "./components/movie/MovieCard";
 
 const App = () => {
-  const [movies, setMovies] = useState([])
-  const [searchKey, setSearchKey] = useState("")
-  const API_URL = "https://api.themoviedb.org/3/"
+  const PATH_URL = "https://image.tmdb.org/t/p/original";
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [searchKey, setSearchKey] = useState("");
+  const API_URL = "https://api.themoviedb.org/3/";
 
   const fetchMovies = async () => {
-    
-    
-    const { data: { results } } = await axios.get(`${API_URL}discover/tv`,
-      {
-        params: {
-          api_key: process.env.REACT_APP_MOVIE_API_KEY,
-          
-        }
-      })
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}discover/tv`, {
+      params: {
+        api_key: process.env.REACT_APP_MOVIE_API_KEY,
+      },
+    });
 
-
-    setMovies(results)
-
-  }
+    setSelectedMovie(results[0]);
+    setMovies(results);
+  };
   const fetchMoviesSearch = async (searchKey) => {
-    const { data: { results } } = await axios.get(`${API_URL}search/tv`,
-      {
-        params: {
-          api_key: process.env.REACT_APP_MOVIE_API_KEY,
-          query: searchKey
-        }
-      })
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}search/tv`, {
+      params: {
+        api_key: process.env.REACT_APP_MOVIE_API_KEY,
+        query: searchKey,
+      },
+    });
 
-
-    setMovies(results)
-
-  }
+    setSelectedMovie(results[0]);
+    setMovies(results);
+  };
 
   useEffect(() => {
-    fetchMovies()
-  }, [])
+    fetchMovies();
+  }, []);
 
   const searchMovies = (e) => {
-    e.preventDefault()
-    fetchMoviesSearch(searchKey)
-  }
+    e.preventDefault();
+    fetchMoviesSearch(searchKey);
+  };
 
   return (
-    <div >
-      <header>
-        <h1>Hola Escuela de Código C13</h1>
+    <div>
+      <header className="header-content">
+        <h1>Pelis Código C13</h1>
         <form onSubmit={searchMovies}>
-
-          <input type='text' onChange={(e) => setSearchKey(e.target.value)} />
-          <button type='submit'>Submit</button>
+          <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
+          <button type="submit">Submit</button>
         </form>
       </header>
-      <div className='container-movies'>
-        {
-          movies.map(movie => (
-            <div key={movie.id}>
-              <MovieCard
-
-                movieInfo={movie}
-              />
-            </div>
-
-
-
-
-
-          ))
-        }
-
-
+      <div className="container-movie" style={{ backgroundImage: `url(${PATH_URL}${selectedMovie.backdrop_path})` }}>
+        <div className="movie-content" >
+          <button className="button-trailer">Play Trailer</button>
+          <h1 className="movie-title">
+            {selectedMovie.name}
+          </h1>
+          {selectedMovie.overview
+            ? <p className="description-movie">{selectedMovie.overview}</p>
+            : null}
+        </div>
       </div>
-
+      <div className="container-movies">
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <MovieCard movieInfo={movie} />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
